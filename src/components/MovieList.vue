e06e6e182baad3277b4b170c66858d752a210962
mahi0302 <gunnam.mahendra3@gmail.com>
2023-11-07 15:57:33 +0530

    v-for directive and :key attribute

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,10 +1,34 @@
 <template>
-  <div id="movie-container">Movie list</div>
+  <div id="movie-container">Movie list
+
+	<ul>
+		<li style="color: white;" v-for="(movie, index) in movies" :key="movie.id">
+			Index:{{ index }} {{ movie.name }}
+		</li>
+	</ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+	data() {
+		return {
+			movies: [
+				{
+					id: 1,
+					name: 'movie-1'
+				}, 
+				{
+					id: 2,
+					name: 'movie-2'
+				},
+				{
+					id: 3,
+					name: 'movie-3'
+				},
+			]
+		}
+	}
 }
 </script>
 
