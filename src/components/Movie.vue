731f28cc3edea22d9126beadcae22400ac354eba
mahi0302 <gunnam.mahendra3@gmail.com>
2023-11-07 18:53:49 +0530

    Adding Search functionality to make interactiive

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -4,7 +4,7 @@
     <div class="description">
         <div class="basic-info">
             <h3>{{ movie.name }}</h3>
-            <span>{{ movie.rating }}</span>
+            <span :style="{'background-color': getRatingColor()}">{{ movie.rating }}</span>
         </div>
         <p>{{ movie.genre }}</p>
         <p>{{ movie.year }}</p>
@@ -18,6 +18,15 @@ export default {
         movie: {
             type: Object,
             default: () => {}
+        },
+    },
+    methods: {
+        getRatingColor() {
+            if(this.movie.rating > 7) return '#5eb85e';
+
+            if(this.movie.rating > 4) return '#ffa809';
+
+            return '#e10505';
         },
     },
 };
