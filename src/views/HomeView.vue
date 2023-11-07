2dffa7e9aacc950cf6b57dcf24b5479247fa5b39
mahi0302 <gunnam.mahendra3@gmail.com>
2023-11-07 15:39:34 +0530

    deleted logo path and Creating navbar, sidebar, movielist and filters component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <navbar />
+
+    <div id="main-section">
+      <sidebar />
+      <movie-list />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import Sidebar from '@/components/Sidebar.vue';
+import MovieList from '@/components/MovieList.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Navbar,
+    Sidebar,
+    MovieList,
   }
 }
 </script>
+
+<style lang="scss" scoped>
+	.home {
+		display: flex;
+		flex-direction: column;
+
+		#main-section {
+			display: flex;
+			margin-top: 50px;
+		}
+	}
+</style>
