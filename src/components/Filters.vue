ca85064843e50b39b31c587deb71c2e8e65669df
mahi0302 <gunnam.mahendra3@gmail.com>
2023-11-08 10:39:51 +0530

    Filters to make interactive

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="filters">
         <h2>Filters</h2>
-        <p>Rating highest</p>
-        <p>Rating lowest</p>
-        <p>Year newest</p>
-        <p>Year oldest</p>
+        <p v-for="(filter, index) in filters" :key="index" @click="applyFilter(filter)">
+			{{ filter.name }}
+		</p>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+	data() {
+		return {
+			filters: [
+				{
+					name: "Rating highest",
+					key: "rating",
+					order: "desc"
+				},
+				{
+					name: "Rating lowest",
+					key: "rating",
+					order: "asc"
+				},
+				{
+					name: "Year newest",
+					key: "year",
+					order: "desc"
+				},
+				{
+					name: "Year oldest",
+					key: "year",
+					order: "asc"
+				},
+			],
+		};
+	},
+	methods: {
+		applyFilter(filter) {
+			this.$store.dispatch('filter', filter);
+		},
+	},
+};
 </script>
 
 <style lang='scss' scoped>
